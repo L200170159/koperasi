59c7d7a95d28318de98820aca2d1e4197a0e81e1
Giovanny Dani Saputra <47962353+L200170159@users.noreply.github.com>
2020-07-12 19:59:06 +0700

    partial user

---
+++ application/views/user/partial/css.php
@@ -0,0 +1,18 @@
+<!-- Favicons -->
+<link href="<?=base_url() ?>assets/img/favicon.png" rel="icon">
+<link href="<?=base_url() ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i,900" rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+<link href="<?=base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?=base_url() ?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+<link href="<?=base_url() ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="<?=base_url() ?>assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+<link href="<?=base_url() ?>assets/vendor/venobox/venobox.css" rel="stylesheet">
+<link href="<?=base_url() ?>assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+<link href="<?=base_url() ?>assets/vendor/aos/aos.css" rel="stylesheet">
+
+<!-- Template Main CSS File -->
+<link href="<?=base_url() ?>assets/css/style.css" rel="stylesheet">
